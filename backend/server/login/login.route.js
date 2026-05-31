1c5bd1770da41f7d57ed61fb4849b7dc7fae4570
techsavvyanuj <anujmishra9893@gmail.com>
2026-05-31 20:47:37 +0530

    Remove extra Flutter platform folders - keep only frontend

--- backend/server/login/login.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const route = express.Router();
-
-const LoginController = require("./login.controller");
-
-route.get("/authorizeLogin", LoginController.get);
-
-module.exports = route;
